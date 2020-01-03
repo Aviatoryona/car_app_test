fafaca7d9c4464995ba3a3f68b9d5229ca2da582
kamaaTheDj <kamaudavikiruku@gmail.com>
2020-01-03 09:40:40 +0300

    :zap: minor fix

--- lib/src/pages/hireCar_page.dart
+++ lib/src/pages/hireCar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 
 
@@ -12,82 +13,82 @@ class HireCar extends StatelessWidget {
     return ListView(
       controller: controller,
           children: <Widget>[
-            Card(
-        elevation: 10,
-        margin: EdgeInsets.all(20),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
-          child: Column(
-            children: <Widget>[
-              Text("₤1821",textAlign: TextAlign.left,style: TextStyle(color: Colors.amber,fontSize: 20),),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Driving Test Center')),
-                  Expanded(child: Text('Aberdeen Ab123gq UK',style: TextStyle(fontWeight: FontWeight.w800),))
-                ],
+                Card(
+                    elevation: 10,
+                    margin: EdgeInsets.all(20),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
+                      child: Column(
+                        children: <Widget>[
+                          Text("₤1821",textAlign: TextAlign.left,style: TextStyle(color: Colors.amber,fontSize: 20),),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Driving Test Center')),
+                              Expanded(child: Text('Aberdeen Ab123gq UK',style: TextStyle(fontWeight: FontWeight.w800),))
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Test Date',textAlign: TextAlign.left,),flex: 2,),
+                              Expanded(child: Text('2020-19-11',style: TextStyle(fontWeight: FontWeight.w800),))
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Test Time',textAlign: TextAlign.left,),flex: 2,),
+                              Expanded(child: Text('00-11-01',style: TextStyle(fontWeight: FontWeight.w800),))
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Transmision',textAlign: TextAlign.left,),flex: 2,),
+                              Expanded(child: Text('Manual',style: TextStyle(fontWeight: FontWeight.w800),),flex: 1,)
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Distance (from pickup address)',textAlign: TextAlign.left,),flex: 2,),
+                              Expanded(child: Text('3.37 miles',style: TextStyle(fontWeight: FontWeight.w800),),flex: 1,)
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(child: Text('Pick up address',textAlign: TextAlign.left,),flex: 2,),
+                              Expanded(child: Text('HAO 2FN',style: TextStyle(fontWeight: FontWeight.w800),))
+                            ],
+                          ),
+                          ButtonBar(
+                            
+                            alignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              FlatButton(
+                                
+                                child: Text('More info...'),
+                                onPressed: (){},
+                              ),
+                              FlatButton(
+                                color: Colors.amber,
+                                child: Text('Claim job.'),
+                                onPressed: (){},
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
               ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Test Date',textAlign: TextAlign.left,),flex: 2,),
-                  Expanded(child: Text('2020-19-11',style: TextStyle(fontWeight: FontWeight.w800),))
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Test Time',textAlign: TextAlign.left,),flex: 2,),
-                  Expanded(child: Text('00-11-01',style: TextStyle(fontWeight: FontWeight.w800),))
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Transmision',textAlign: TextAlign.left,),flex: 2,),
-                  Expanded(child: Text('Manual',style: TextStyle(fontWeight: FontWeight.w800),),flex: 1,)
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Distance (from pickup address)',textAlign: TextAlign.left,),flex: 2,),
-                  Expanded(child: Text('3.37 miles',style: TextStyle(fontWeight: FontWeight.w800),),flex: 1,)
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(child: Text('Pick up address',textAlign: TextAlign.left,),flex: 2,),
-                  Expanded(child: Text('HAO 2FN',style: TextStyle(fontWeight: FontWeight.w800),))
-                ],
-              ),
-              ButtonBar(
-                
-                alignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  FlatButton(
-                    
-                    child: Text('More info...'),
-                    onPressed: (){},
-                  ),
-                  FlatButton(
-                    color: Colors.amber,
-                    child: Text('Claim job.'),
-                    onPressed: (){},
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
 
 
 
